7b19f6dd2f5c2a72a261720a919d23cb7ef0e655
Iverson Diles <ivcotad@gmail.com>
2022-02-19 20:38:46 -0700

    graphql settings

---
+++ config/plugins.js
@@ -0,0 +1,17 @@
+// path: ./config/plugins.js
+
+module.exports = {
+    //
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: true,
+        depthLimit: 7,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      },
+    },
+  };
